fc39fd30ce0d114f441d285ced34ead08df105d8
Thomas Rix <tom@rixth.org>
2012-03-05 22:46:37 -0800

    Integrate the new game controls

--- public/app.js
+++ public/app.js
@@ -1,7 +1,11 @@
 (function ($, undefined) {
   "use strict";
 
-  var board = $('#board');
+  var board = $('#board'),
+      size = 8,
+      difficulty = 'NORMAL',
+      currentGame,
+      gameState = {}
 
   function resizeCells() {
     var tds = board.find('td');
@@ -14,4 +18,32 @@
   board.on('click', 'td', function () {
     $(this).toggleClass('marked');
   });
+
+  $('#newGameControl').on('click', 'ul li, div>a:first-child', function (event) {
+    promptNewGame((function () {
+      // Select the difficulty in the list
+      if (this.tagName.toUpperCase() === 'LI') {
+        $(this).siblings().removeClass('active').end().addClass('active');
+      }
+      currentGame = new MinesweeperGame(size, difficulty);
+    }).bind(this));
+  }).find("li[data-difficulty='" + difficulty + "']").addClass('active');
+
+  $('#sizeSelector').on('change', function () {
+    promptNewGame((function () {
+      size = $(this).val();
+      currentGame = new MinesweeperGame(size, difficulty);
+    }).bind(this), (function () {
+      $(this).val(size);
+    }).bind(this));
+  }).val(8);
+
+  function promptNewGame(continueCallback, cancelCallback) {
+    if (false) {
+      // Prompt for continue, then call callback
+
+    } else {
+      continueCallback();
+    }
+  }
 })(jQuery);
